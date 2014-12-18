16335553405474c9c132ae3a23bb241d6bace6c5
mpintar <mark.pintar@outlook.com>
2014-12-17 22:01:38 -0500

    Updating the MathE functions
    Added a proper check for the potential divide by zero in Map to avoid
    floating point errors.
    
    MapAndClamp() now does a check to see if max2 is greater or less than
    min2 so it knows how to properly clamp the value.
    
    Added in a Normalize function

--- Assets/Utilities/MathE.cs
+++ Assets/Utilities/MathE.cs
@@ -7,9 +7,11 @@ namespace UnityUtilities
     {
         public static float Map(float value, float min1, float max1, float min2, float max2)
         {
-            if (max1 - min1 == 0)
+            if (Mathf.Abs(max1 - min1) < Mathf.Epsilon)
             {
-                return 0;
+                Debug.LogWarning("MathE.Map(): Potential divide by zero. Check the min1: " + min1 + " and max1: " + max1 +
+                    "\n returning min2");
+                return min2;
             }
 
             return (value - min1) / (max1 - min1) * (max2 - min2) + min2;
@@ -19,7 +21,17 @@ namespace UnityUtilities
         {
             var mappedValue = Map(value, min1, max1, min2, max2);
 
-            return Mathf.Clamp(mappedValue, min2, max2);
+            if (min2 < max2)
+            {
+                return Mathf.Clamp(mappedValue, min2, max2);
+            }
+
+            return Mathf.Clamp(mappedValue, max2, min2);
+        }
+
+        public static float Normalize(float value, float min, float max)
+        {
+            return Mathf.Clamp01((value - min) / (max - min));
         }
 
         public static int GetByteFromNormalizedFloat(float normalizedValue)
